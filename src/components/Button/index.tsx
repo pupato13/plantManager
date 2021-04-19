186d5c3145d2c0a97e729f82cb9fd507281b335b
diego@layerx.co.nz <Palmeiras1914>
2021-04-20 08:15:16 +1200

    refactor: move button to folder using index file

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { TouchableOpacity, Text, StyleSheet } from "react-native";
-import colors from "../styles/colors";
+import colors from "../../styles/colors";
 
 interface IButtonProps {
     text: string;
