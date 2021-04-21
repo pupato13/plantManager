1c908968a1b40de7ba45054ba7d5623f77fdeda7
Diego Pupato <pupato.diego@gmail.com>
2021-04-21 20:27:34 +1200

    feat: add google fonts and appLoading to hold SplashScreen until fonts is loading

--- App.tsx
+++ App.tsx
@@ -1,6 +1,21 @@
 import React from "react";
+import {
+    useFonts,
+    Jost_400Regular,
+    Jost_600SemiBold,
+} from "@expo-google-fonts/jost";
+import AppLoading from "expo-app-loading";
 import Welcome from "./src/screens/Welcome";
 
 export default function App() {
+    const [isFontsLoaded] = useFonts({
+        Jost_400Regular,
+        Jost_600SemiBold,
+    });
+
+    if (!isFontsLoaded) {
+        return <AppLoading />;
+    }
+
     return <Welcome />;
 }
