e3783d9ff5d21c8c48c0aa6d706c37f2ddf44ad5
Diego Pupato <pupato.diego@gmail.com>
2021-04-24 15:33:00 +1200

    feat: finish MyPlant screen

--- src/screens/MyPlants.tsx
+++ src/screens/MyPlants.tsx
@@ -1,10 +1,63 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Image, FlatList } from "react-native";
+import { formatDistance } from "date-fns";
+import { enNZ } from "date-fns/locale";
+import Header from "../components/Header";
+import colors from "../styles/colors";
+import waterdrop from "../assets/waterdrop.png";
+import { IPlantProps } from "../types/plant";
+import { getPlants } from "../libs/storage";
+import fonts from "../styles/fonts";
+import PlantCardSecondary from "../components/PlantCardSecondary";
 
 const MyPlants = () => {
+    const [plants, setPlants] = useState<IPlantProps[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [nextWatered, setNextWatered] = useState<string>();
+
+    useEffect(() => {
+        async function loadStorageData() {
+            const storedPlants = await getPlants();
+
+            const nextTime = formatDistance(
+                new Date(storedPlants[0].dateTimeNotification).getTime(),
+                new Date().getTime(),
+                { locale: enNZ }
+            );
+
+            setNextWatered(
+                `Don't forget to water ${storedPlants[0].name} at ${nextTime}`
+            );
+
+            setPlants(storedPlants);
+            setIsLoading(false);
+        }
+
+        loadStorageData();
+    }, []);
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>MyPlants</Text>
+            <Header />
+
+            <View style={styles.spotlight}>
+                <Image source={waterdrop} style={styles.spotlightImage} />
+                <Text style={styles.spotlightText}>{nextWatered}</Text>
+            </View>
+
+            <View style={styles.plants}>
+                <Text style={styles.plantsTitle}>Next Watered</Text>
+
+                <FlatList
+                    keyExtractor={(plant) => String(plant.id)}
+                    data={plants}
+                    renderItem={({ item }) => (
+                        <PlantCardSecondary data={item} />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{ flex: 1 }}
+                />
+            </View>
         </View>
     );
 };
@@ -12,10 +65,39 @@ const MyPlants = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 16,
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingHorizontal: 32,
+        paddingTop: 48,
+        backgroundColor: colors.background,
     },
-    title: {
-        fontSize: 16,
+    spotlight: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundColor: colors.blue_light,
+        paddingHorizontal: 20,
+        borderRadius: 20,
+        height: 110,
+    },
+    spotlightImage: {
+        width: 56,
+        height: 56,
+    },
+    spotlightText: {
+        flex: 1,
+        color: colors.blue,
+        paddingHorizontal: 20,
+    },
+    plants: {
+        flex: 1,
+        width: "100%",
+    },
+    plantsTitle: {
+        fontSize: 24,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        marginVertical: 20,
     },
 });
 
