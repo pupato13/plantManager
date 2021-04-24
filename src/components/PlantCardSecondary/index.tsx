0d83a2aa679a5f40598396a02efe01a105da4477
Diego Pupato <pupato.diego@gmail.com>
2021-04-25 07:43:10 +1200

    feat: add button remove and swipeable animation

--- src/components/PlantCardSecondary/index.tsx
+++ src/components/PlantCardSecondary/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import Animated from "react-native-reanimated";
+import { Feather } from "@expo/vector-icons";
 import colors from "../../styles/colors";
 import fonts from "../../styles/fonts";
 import { SvgFromUri } from "react-native-svg";
@@ -11,18 +14,43 @@ interface IPlantProps extends RectButtonProps {
         photo: string;
         hour: string;
     };
+    handleRemove: () => void;
 }
 
-const PlantCardSecondary: React.FC<IPlantProps> = ({ data, ...rest }) => {
+const PlantCardSecondary: React.FC<IPlantProps> = ({
+    data,
+    handleRemove,
+    ...rest
+}) => {
     return (
-        <RectButton style={styles.container} {...rest}>
-            <SvgFromUri uri={data.photo} width={48} height={48} />
-            <Text style={styles.title}>{data.name}</Text>
-            <View style={styles.details}>
-                <Text style={styles.timeLabel}>Water at</Text>
-                <Text style={styles.time}>{data.hour}</Text>
-            </View>
-        </RectButton>
+        <Swipeable
+            overshootRight={false}
+            renderRightActions={() => (
+                <Animated.View>
+                    <View>
+                        <RectButton
+                            style={styles.buttonRemove}
+                            onPress={handleRemove}
+                        >
+                            <Feather
+                                name="trash"
+                                size={32}
+                                color={colors.white}
+                            />
+                        </RectButton>
+                    </View>
+                </Animated.View>
+            )}
+        >
+            <RectButton style={styles.container} {...rest}>
+                <SvgFromUri uri={data.photo} width={48} height={48} />
+                <Text style={styles.title}>{data.name}</Text>
+                <View style={styles.details}>
+                    <Text style={styles.timeLabel}>Water at</Text>
+                    <Text style={styles.time}>{data.hour}</Text>
+                </View>
+            </RectButton>
+        </Swipeable>
     );
 };
 
@@ -57,6 +85,19 @@ const styles = StyleSheet.create({
         fontSize: 16,
         fontFamily: fonts.heading,
         color: colors.body_dark,
+    },
+    buttonRemove: {
+        width: 100,
+        height: 85,
+        backgroundColor: colors.red,
+        marginTop: 10,
+        borderRadius: 20,
+        alignItems: "center",
+        justifyContent: "center",
+        position: "relative",
+        right: 20,
+        // paddingRight: 16,
+        paddingLeft: 16,
     },
 });
 
