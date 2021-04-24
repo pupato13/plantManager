6cd5796c52f181840d0e4d4f964c37620110405e
Diego Pupato <pupato.diego@gmail.com>
2021-04-25 08:17:09 +1200

    feat: add notifications when saving plant

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -1,22 +1,57 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { format } from "date-fns";
+import * as Notifications from "expo-notifications";
 import { PlantsKey } from "../types/asyncStorageKeys";
 import { IPlantProps } from "../types/plant";
 
 export interface IStoragePlantProps {
     [id: string]: {
         data: IPlantProps;
+        notificationId: string;
     };
 }
 
 export async function savePlant(plant: IPlantProps): Promise<void> {
     try {
+        const nextTime = new Date(plant.dateTimeNotification);
+        const now = new Date();
+
+        const { times, repeat_every } = plant.frequency;
+
+        if (repeat_every === "week") {
+            const interval = Math.trunc(7 / times);
+            nextTime.setDate(now.getDate() + interval);
+        } else {
+            nextTime.setDate(nextTime.getDate() + 1);
+        }
+
+        const seconds = Math.abs(
+            Math.ceil(now.getTime() - nextTime.getTime()) / 1000
+        );
+
+        const notificationId = await Notifications.scheduleNotificationAsync({
+            content: {
+                title: "Heeey, 🌱",
+                body: `It's time to water your ${plant.name}`,
+                sound: true,
+                priority: Notifications.AndroidNotificationPriority.HIGH,
+                data: {
+                    plant,
+                },
+            },
+            trigger: {
+                seconds: seconds < 60 ? 60 : seconds,
+                repeats: true,
+            },
+        });
+
         const data = await AsyncStorage.getItem(PlantsKey);
         const oldPlants = data ? (JSON.parse(data) as IStoragePlantProps) : {};
 
         const newPlant = {
             [plant.id]: {
                 data: plant,
+                notificationId,
             },
         };
 
